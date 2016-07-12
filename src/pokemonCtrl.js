8ac1456f303dedbce558173f575cdbcdb05e6225
Matt Pedersen <mpedersen2054@gmail.com>
2016-07-12 15:46:21 -0500

    add code for pokemonCtrl: .all(), .byAlphabetical(), .byType(), .byStat()

--- src/pokemonCtrl.js
+++ src/pokemonCtrl.js
@@ -4,27 +4,54 @@ var pokeData = require('../data/pokemonList.json');
 var Pokemon = (function() {
 
   var all = function() {
+    var pokemon = _.clone(pokeData);
+    var sorted  = _.sortBy(pokemon, function(p) {
+      return +p.natNum;
+    });
     return {
-      by: pokeData.length
+      by: sorted
     }
   }
 
   var byAlphabetical = function() {
+    var pokemon = _.clone(pokeData);
+    var sorted  = _.sortBy(pokemon, function(p) {
+      return p.name;
+    });
     return {
-      by: 'alphabetical'
+      by: sorted
     }
   }
 
   // types=[]
   var byType = function(types) {
+    var pokemon  = _.clone(pokeData);
+    var filtered = _.filter(pokemon, function(p) {
+      // if there are 2 types make sure both are in there
+      // else just make sure the 1 type is in types array
+      if (types.length > 1) {
+        if (_.includes(p.types, types[0]) && _.includes(p.types, types[1])) {
+          return p;
+        }
+      } else {
+        if (_.includes(p.types, types[0])) {
+          return p;
+        }
+      }
+    })
+    // show stronger first
+    var sorted = _.reverse(_.sortBy(filtered, 'bst'));
     return {
-      by: 'type'
+      by: sorted
     }
   }
 
-  var byStat = function() {
+  var byStat = function(stat) {
+    var pokemon = _.clone(pokeData);
+    // show stronger first
+    var sorted  = _.reverse(_.sortBy(pokemon, stat));
     return {
-      by: 'stat'
+      by: sorted
     }
   }
 
